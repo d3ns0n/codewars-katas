32a95e6c8c8d0def700f515bf10312c32959710c
D3ns0n <d3ns0n@d3ns0n.de>
2019-02-05 22:34:33 +0100

    add Kata: double-cola (improved solution)

--- double-cola/src/main/java/Line.java
+++ double-cola/src/main/java/Line.java
@@ -1,27 +1,11 @@
 public class Line {
 
     public static String WhoIsNext(String[] names, int n) {
-        int round = 0;
-        int queueMultiplier = 1;
-        int queueSize = names.length;
-        int cansLeft = n;
-
-        while (queueSize < cansLeft) {
-            round++;
-            cansLeft = cansLeft - queueSize;
-            queueMultiplier = calculateQueueMultiplier(round);
-            queueSize = names.length * queueMultiplier;
+        while (n > names.length) {
+            n = (n - (names.length - 1)) / 2;
         }
 
-        return names[calculateIndex(queueMultiplier, cansLeft)];
-    }
-
-    private static int calculateQueueMultiplier(int round) {
-        return (int) Math.pow(2, round);
-    }
-
-    private static int calculateIndex(int queueMultiplier, int cansLeft) {
-        return Math.toIntExact((cansLeft - 1) / queueMultiplier);
+        return names[n - 1];
     }
 
 }
