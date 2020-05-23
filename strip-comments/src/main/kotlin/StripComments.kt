0fee0eea5958e6850935806c767ed3e15310265f
D3ns0n <d3ns0n@d3ns0n.de>
2020-05-23 11:15:13 +0200

    add Kata: Strip Comments - improved version

--- strip-comments/src/main/kotlin/StripComments.kt
+++ strip-comments/src/main/kotlin/StripComments.kt
@@ -1,19 +1,2 @@
-import kotlin.streams.toList
-
-fun solution(input: String, markers: CharArray): String {
-    val lineBreak = "\n"
-
-    return input.split(lineBreak).stream()
-        .map { stripComment(it, markers) }
-        .map { it.trim() }
-        .toList().joinToString(lineBreak)
-}
-
-fun stripComment(input: String, markers: CharArray): String {
-    for(marker in markers) {
-        val indexOf = input.indexOf(marker)
-        if (indexOf != -1) return input.substring(0 until indexOf)
-    }
-
-    return input
-}
+fun solution(input: String, markers: CharArray) =
+    input.lines().joinToString("\n") { line -> line.split(*markers).first().trimEnd() }
